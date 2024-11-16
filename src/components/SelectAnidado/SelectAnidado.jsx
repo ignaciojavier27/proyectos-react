18c30b1e7a4cf2c10465496bd051ac30ef06d5a0
ignaciojavier27 <nacholin124@gmail.com>
2024-11-16 18:15:58 -0300

    Inicio select anidados

---
+++ src/components/SelectAnidado/SelectAnidado.jsx
@@ -0,0 +1,11 @@
+import Titulo from "../Titulo"
+
+const SelectAnidado = () => {
+    return (
+        <article className="article-project">
+            <Titulo titulo="Select Anidado" />
+        </article>
+    )
+}
+
+export default SelectAnidado
